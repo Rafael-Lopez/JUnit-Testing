3b9711421c70fcd02d1bc839fa665042fbe7f042
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-05-03 00:52:17 -0400

    Adding Client + Address + ClientTest

---
+++ src/main/java/Client.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    private List<Address> addresses;
+
+    public Client(){
+        this.addresses = new ArrayList<Address>();
+    }
+
+    public List<Address> getAddresses(){
+        return addresses;
+    }
+
+    public void addAddress(Address address){
+        addresses.add(address);
+    }
+}
