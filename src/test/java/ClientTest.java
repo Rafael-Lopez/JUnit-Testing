0b61929cc8410c96630bc991e7f0477f85c65bca
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-05-03 00:55:43 -0400

    Making client variable a Test Fixture

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -6,26 +6,23 @@ public class ClientTest {
 
     private Address addressA = new Address("Street A");
     private Address addressB = new Address("Street B");
+    private Client client = new Client();
 
     @Test
     public void afterCreationShouldHaveNoAddress(){
-        Client client = new Client();
-
         assertEquals(0, client.getAddresses().size());
     }
 
     @Test
     public void shouldAllowToAddAddress(){
-        Client client = new Client();
         client.addAddress(addressA);
-
+        
         assertEquals(1, client.getAddresses().size());
         assertTrue(client.getAddresses().contains(addressA));
     }
-    
+
     @Test
     public void shouldAllowToAddManyAddresses(){
-        Client client = new Client();
         client.addAddress(addressA);
         client.addAddress(addressB);
 
