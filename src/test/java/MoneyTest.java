993fb988d49d4ad393d80757c8a254139ae03474
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-05-03 00:14:09 -0400

    Adding JUnitParams dependency +  data-providing method

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,15 +1,28 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(JUnitParamsRunner.class)
 public class MoneyTest {
 
-    @Test
-    public void constructorShouldSetAmountAndCurrency(){
-        Money money = new Money(10, "USD");
+    private final Object[] getMoney(){
+        return  new Object[]{
+          new Object[]{10, "USD"},
+          new Object[]{20, "EUR"}
+        };
+    }
 
-        assertEquals(10, money.getAmount());
-        assertEquals("USD", money.getCurrency());
+    @Test
+    @Parameters(method = "getMoney")
+    public void constructorShouldSetAmountAndCurrency(int amount, String currency){
+        Money money = new Money(amount, currency);
+
+        assertEquals(amount, money.getAmount());
+        assertEquals(currency, money.getCurrency());
         assertNotNull(money);
     }
 }
